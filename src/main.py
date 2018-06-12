58f39f4ab15882629f6fdb0b2e07b735c691a3c3
TheodoreChristakis <theodorechristakis@gmail.com>
2018-06-12 15:58:00 -0400

    Successes and failures added to main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import vis as v
 import spacy
 
 nlp = spacy.load('en')
-
+failures = 0
+successes = 0
 with open("../squad2/dev-v2.0.json", encoding = "utf8") as f:
 	dataset_json = json.load(f)
 	dataset = dataset_json['data']
@@ -15,9 +16,13 @@ for topic in dataset:
 			if not span['is_impossible'] and len(span['question']) < 60:
 				print(span['question'])
 				shortAnswer = None
+				failures++
 				for answerBlob in span['answers']:
 					ans = answerBlob['text']
 					if shortAnswer == None or len(ans) < len(shortAnswer):
 						shortAnswer = ans
+						successes++
 				sentence = list(nlp(span['question']).sents)[0]
 				print(v.parse(sentence, shortAnswer))
+print("Number of failures: ", failures, "\n")
+print("Number of successes: ", successes, "\n")
